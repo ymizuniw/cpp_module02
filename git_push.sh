3cb549afc76b18017220d63f54a9a5cd8fdd1dcd
Yugo Mizuniwa <ymizuniw@c6r2s14.42tokyo.jp>
2025-12-12 19:02:36 +0900

    ref

---
+++ git_push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read -r -p "branch: " BRANCH
+git checkout "$BRANCH"
+git add .
+read -r -p "commit message: " MSG
+git commit -m "$MSG"
+read -r -p "push?[y/n]" PUSH
+if [ "$PUSH" == "y" ]; then
+    git push $BRANCH
+else
+    exit 1
+fi
