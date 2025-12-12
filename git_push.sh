6c78c65f5a867656b12fa98d7da502c5487a2d3e
Yugo Mizuniwa <ymizuniw@c6r2s14.42tokyo.jp>
2025-12-12 19:06:47 +0900

    ref

--- git_push.sh
+++ git_push.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-read -r -p "remote: " REMOTE
 read -r -p "branch: " BRANCH
 git checkout "$BRANCH"
 git add .
 read -r -p "commit message: " MSG
 git commit -m "$MSG"
+read -r -p "remote: " REMOTE
 read -r -p "push?[y/n]" PUSH
 if [ "$PUSH" == "y" ]; then
     git push "$REMOTE $BRANCH"
